9fdbdd4fe7797c515577c5691484f10efa20baec
BhartiGirdhar <bhartigirdhar1999@gmail.com>
2024-11-21 14:27:56 +0530

    Queries Quality and Percentage Solution

---
+++ Basic Aggregate Functions/1211. Queries Quality and Percentage.sql
@@ -0,0 +1,4 @@
+Select query_name, ROUND(AVG(rating/position),2) as quality, ROUND(AVG(if(rating <3, 1, 0))*100,2) as poor_query_percentage
+from Queries
+where query_name is not null
+Group by query_name;
